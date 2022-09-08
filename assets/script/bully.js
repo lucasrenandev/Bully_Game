a53cc82177d06eaec8ef70d3c89048763b07612f
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-08 20:10:04 -0300

    Bully Game - Website

---
+++ assets/script/bully.js
@@ -0,0 +1,20 @@
+
+const button = document.getElementById("button")
+
+button.addEventListener("click", function () {
+    window.scrollTo(0, 0)
+})
+
+document.addEventListener("scroll", Hidden)
+
+function Hidden() {
+
+    if(window.scrollY > 1200) {
+        button.style.display = "flex"
+    }
+    else {
+        button.style.display = "none"
+    }
+}
+
+Hidden()
