b9051806847add65ea66bb70e83b86853ec8f0b1
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-14 15:11:35 -0300

    Update bully.js

--- assets/script/bully.js
+++ assets/script/bully.js
@@ -1,32 +1,4 @@
 
-/* == SCRIPT PARA O SCROLL DOS LINKS DE NAVEGAÇÃO == */
-const menuItems = document.querySelectorAll("nav a")
-menuItems.forEach(item => {
-    item.addEventListener("click", scrollId_OnClick)
-})
-
-/* rolagem dos links de navegação por ID */
-function scrollId_OnClick(event) {
-    event.preventDefault()
-    const scroll = getScrollByHref(event.target) - 120
-    scroll_Smooth(scroll)
-}
-
-/* scroll suave */
-function scroll_Smooth(scroll) {
-    window.scroll({
-        top: scroll,
-        behavior: "smooth"
-    })
-}
-
-/* pegando os links de navegação por Href */
-function getScrollByHref(element) {
-    const id = element.getAttribute("href")
-    return document.querySelector(id).offsetTop
-}
-
-/* == SCRIPT PARA O SCROLL DO BOTÃO == */
 const button = document.getElementById("button")
 button.addEventListener("click", function () {
     window.scrollTo(0, 0)
