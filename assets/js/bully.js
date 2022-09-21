53b30be6f6c887f0de6104613c743261baa5ad55
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-21 14:59:14 -0300

    Update Website

---
+++ assets/js/bully.js
@@ -0,0 +1,34 @@
+
+// Scroll
+const button = document.getElementById("button")
+button.addEventListener("click", function() {
+    window.scrollTo(0, 0)
+})
+
+document.addEventListener("scroll", Hidden)
+function Hidden() {
+    if(window.scrollY > 1000) {
+        button.style.display = "flex"
+    }
+    else {
+        button.style.display = "none"
+    }
+}
+Hidden()
+
+// Mobile menu
+const menu = document.querySelector(".menu")
+const menuImage = document.querySelector(".menu img")
+const navegation = document.querySelector("nav ul")
+menu.addEventListener("click", openMenu)
+
+function openMenu() {
+    navegation.classList.toggle("active")
+
+    if(menuImage.getAttribute("src") === "assets/images/open.svg") {
+        menuImage.setAttribute("src", "assets/images/close.svg")
+    }
+    else {
+        menuImage.setAttribute("src", "assets/images/open.svg")
+    }
+}
